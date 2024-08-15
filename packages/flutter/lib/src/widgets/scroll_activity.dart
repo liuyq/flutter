34089488856db9449ae3d49207d27c394dbdf166
openharmony_ci <120357966@qq.com>
2024-08-15 00:57:16 +0000

    !294 减小最小滑动距离阈值，提升应用响应速度 Merge pull request !294 from lihui/dev

--- packages/flutter/lib/src/widgets/scroll_activity.dart
+++ packages/flutter/lib/src/widgets/scroll_activity.dart
@@ -387,7 +387,7 @@ class ScrollDragController implements Drag {
                 offset.sign;
           }
         } else {
-          return 0.0;
+          return offset < 0 ? offset - 1.0 : offset + 1.0;
         }
       }
     }
