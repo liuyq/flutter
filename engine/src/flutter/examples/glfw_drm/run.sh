c17932934e179096f4b617036946e66b081f8cab
Yongqin Liu <yongqin.liu@linaro.org>
2026-01-15 17:38:10 +0800

    engineL: glfw_drm: run.sh: create myapp in tempory directory
    to avoid the error that creating a project within
    the Flutter SDK.
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- engine/src/flutter/examples/glfw_drm/run.sh
+++ engine/src/flutter/examples/glfw_drm/run.sh
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 set -e # Exit if any program returns an error.
 
+dir_parent=$(cd $(dirname $0); pwd)
 #################################################################
 # Make the host C++ project.
 #################################################################
@@ -17,16 +18,23 @@ make
 #################################################################
 # Make the guest Flutter project.
 #################################################################
-if [ ! -d myapp ]; then
-    flutter create myapp
-    cd myapp
+# Since cannot create a project within the Flutter SDK. Let's create
+# the app project under one temporary directory
+# and use absolute path as much as posisble
+dir_tmp=$(mktemp -d)
+name_app="myapp"
+dir_app="${dir_tmp}/${name_app}"
+cd ${dir_tmp}
+if [ ! -d "${name_app}" ]; then
+    flutter create "${name_app}"
+    cd "${name_app}"
     flutter pub add flutter_spinkit
     cd ..
 fi
-cd myapp
-cp ../../main.dart lib/main.dart
+cd "${name_app}"
+cp ${dir_parent}/main.dart lib/main.dart
 flutter build bundle \
-        --local-engine-src-path ../../../../../ \
+        --local-engine-src-path ${dir_parent}/../../../ \
         --local-engine=host_debug_unopt \
         --local-engine-host=host_debug_unopt
 cd -
@@ -34,4 +42,4 @@ cd -
 #################################################################
 # Run the Flutter Engine Embedder
 #################################################################
-./flutter_glfw ./myapp ../../../third_party/icu/common/icudtl.dat
+${dir_parent}/debug/flutter_glfw ${dir_tmp}/myapp ${dir_parent}/../../third_party/icu/common/icudtl.dat
