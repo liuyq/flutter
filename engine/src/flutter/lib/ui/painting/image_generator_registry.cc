CarSmallGuo/flutter_oshm

APNG动图支持修改
Signed-off-by: lwx0221 <741744990@qq.com>

engine/src/flutter/lib/ui/painting/image_generator_registry.cc
@@ -23,6 +23,11 @@ ImageGeneratorRegistry::ImageGeneratorRegistry() : weak_factory_(this) {
       [](sk_sp<SkData> buffer) {
         return APNGImageGenerator::MakeFromData(std::move(buffer));
       },
+#ifdef __OHOS__
+      // OHOS's PixelMap currently does not support APNG decoding. Direct
+      // decoding will result in the image being decoded as PNG.
+      2);
+#else
       0);
 
   AddFactory(

liuyq/flutter

engine/src/flutter/lib/ui/painting/image_generator_registry.cc
@@ -23,6 +23,11 @@ ImageGeneratorRegistry::ImageGeneratorRegistry() : weak_factory_(this) {
       [](sk_sp<SkData> buffer) {
         return APNGImageGenerator::MakeFromData(std::move(buffer));
       },
+#ifdef FML_OS_OHOS
+      // OHOS's PixelMap currently does not support APNG decoding. Direct
+      // decoding will result in the image being decoded as PNG.
+      2);
+#else
       0);
 
   AddFactory(